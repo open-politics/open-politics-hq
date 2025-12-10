c3b89be2d01841dfa51253dbb9fea9c1aefb7c43
Jim Wagner <jimvwagner@googlemail.com>
2025-12-10 16:35:23 +0100

    included direct execution command example

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -5,6 +5,9 @@ set -e
 # alembic revision --autogenerate -m "adding user preferences"
 alembic upgrade head
 
+# Alternatively if the container is running:
+# docker compose exec backend bash -c "alembeic revision --autogenerate -m 'message' && alembic upgrade head"
+
 # Let the DB start
 python /app/app/backend_pre_start.py
 
