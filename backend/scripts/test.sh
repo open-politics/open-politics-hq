6461675dc6924e12c0ce54a9ab6a0f0cadf3a545
Jim Wagner <jimvwagner@googlemail.com>
2026-03-27 12:01:52 +0100

    updating testing suite

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
-# Run pytest directly
-# pytest "$@"
-
-python /app/app/tests/workflow/test_full_workflow.py
-
-# Removed coverage command:
-# coverage run --source=app -m pytest "$@"
-# coverage report -m
+pytest "$@"
